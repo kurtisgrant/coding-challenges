421390079b7f17d9094a6cc308b3e4613000f673
Kurtis Grant <kurtis.b.grant@gmail.com>
2022-02-27 18:44:42 -0500

    LC-0127

---
+++ 22-02/27/LC-0127.js
@@ -0,0 +1,28 @@
+/*
+  LeetCode #127: 
+  Best Time to Buy and Sell Stock
+
+  https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+  
+  You are given an array prices where prices[i] is the price of a given stock on the ith day.
+
+  You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+
+  Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+
+*/
+
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+  let highest = -Infinity;
+  let lowest = Infinity;
+  
+  for (let i = 0; i < prices.length; i++) {
+    const price = prices[i];
+    for (let j = 1; j < prices.length; j++) {
+      if 
+    }
+};
